0d23af9c728490ca0a30b6a30a0e1a817701d492
sgioia <sgioia@cisco.com>
2019-03-20 12:33:39 +0100

    Changed the usernv source

--- c5suite/appz/wordpress/pre-start-app-script.sh
+++ c5suite/appz/wordpress/pre-start-app-script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -x
-source /etc/user/osmosix/etc/userenv
-
-sudo sed -e "s/%CliqrTier_DSERVER_PUBLIC_IP%/$CliqrTier_DSERVER_PUBLIC_IP/" \
- /var/www/wp-config.php
+source /usr/local/osmosix/etc/userenv
+sudo sed -e "s/%CliqrTier_DSERVER_PUBLIC_IP%/$CliqrTier_DSERVER_PUBLIC_IP/" /var/www/wp-config.php
