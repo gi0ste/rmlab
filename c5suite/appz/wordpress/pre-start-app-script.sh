6e50da4f0b6034f09710ed729eb388e92f160c8f
sgioia <sgioia@cisco.com>
2019-03-22 10:33:51 +0100

    Changed sed -e to sed -i

--- c5suite/appz/wordpress/pre-start-app-script.sh
+++ c5suite/appz/wordpress/pre-start-app-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 source /usr/local/osmosix/etc/userenv
 
-sudo sed -e "s/%CliqrTier_DSERVER_PUBLIC_IP%/$CliqrTier_DSERVER_PUBLIC_IP/" /var/www/wp-config.php
+sudo sed -i "s/%CliqrTier_DSERVER_PUBLIC_IP%/$CliqrTier_DSERVER_PUBLIC_IP/" /var/www/wp-config.php
