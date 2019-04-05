b3ec22290ce7cc2de7a08f858b572e0a1c00a6d6
rtortori <34518425+rtortori@users.noreply.github.com>
2019-04-05 16:43:32 +0200

    support vmware and public cloud attempt #1

--- innovation_hub/apps/rmlab-opencart/install_opencart_cli.sh
+++ innovation_hub/apps/rmlab-opencart/install_opencart_cli.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+exec > >(tee -a /var/tmp/config-opencart-init_$$.log) 2>&1
 
 OSMOSIX_BASE_DIR=/usr/local/osmosix
 OPENCART_INSTALLED_FILE=$OSMOSIX_BASE_DIR/etc/.OPENCART_INSTALLED
+SCRIPTNAME=$(basename $0)
 
-[[ -f $OPENCART_INSTALLED_FILE ]] && { echo "OpenCart already installed"; exit; }
+if [ -f $OPENCART_INSTALLED_FILE ]; then echo "OpenCart already installed"; exit; fi
 
 # ensure running as root
 if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
 
-source /usr/local/osmosix/etc/userenv
+if [ -f /usr/local/osmosix/etc/userenv ]; then source /usr/local/osmosix/etc/userenv; fi
 
+# rtortori
 if [ -z $CliqrTier_Apache_PUBLIC_IP ]; then
   # Added for vmware deployments (no public IP)
   CliqrTier_Apache_PUBLIC_IP=$CliqrTier_Apache_IP
@@ -24,34 +27,202 @@ if [ ! -z $CliqrTier_Database_PUBLIC_IP ]; then
   CliqrTier_Database_IP=$CliqrTier_Database_PUBLIC_IP
 fi
 
-#####added by vinod#######
-IFS=","
-hostname=$cliqrNodeHostname
-INDEX=0
-IPINDEX=0
+Apache_IP=$CliqrTier_Apache_PUBLIC_IP
+[ -z $Apache_IP ] && Apache_IP=$CliqrTier_Apache_IP
+Database_IP=$CliqrTier_Database_IP
 
-for i in $CliqrTier_Apache_HOSTNAME ;
-  do
-    position=${INDEX}
-    if [ $i = $hostname ];
-        then
-          node_position=$position
-    fi
-    let INDEX=${INDEX}+1
+#
+
+if [ "$Cloud_Setting_CloudFamily" != "Vmware" ]; then
+
+  IFS=","
+  hostname=$cliqrNodeHostname
+  INDEX=0
+  IPINDEX=0
+
+  for i in $CliqrTier_Apache_HOSTNAME ;
+    do
+      position=${INDEX}
+      if [ $i = $hostname ];
+          then
+            node_position=$position
+      fi
+      let INDEX=${INDEX}+1
+    done
+  for j in $CliqrTier_Apache_PUBLIC_IP ;
+    do
+      ipposition=${IPINDEX}
+      let IPINDEX=${IPINDEX}+1
+      if [ $ipposition = $node_position ];
+         then
+           HTTP_SERVER=$j;
+  #         echo $HTTP_SERVER;
+      fi
+    done
+
+  cd /var/www/install
+  php cli_install.php install --db_driver mysqli --db_host $CliqrTier_Database_PUBLIC_IP --db_user root --db_password opencart --db_name opencart --username admin --password admin --email ratmistr@cisco.com --agree_tnc yes --http_server http://$HTTP_SERVER/
+  touch $OPENCART_INSTALLED_FILE
+
+exit 0
+fi
+
+
+
+
+if [ -z $Apache_IP ]; then
+  echo Could not determine Web Server IP Address
+  exit 4
+elif [ -z $Database_IP ]; then
+  echo Could not determine Database Server IP Address
+  exit 5
+fi
+
+echo DB: $Database_IP, Web:$Apache_IP
+sleep 1
+
+WEBROOT=/var/www/html
+
+
+if [ -f /var/www/index.php ]; then
+  if [ -d $WEBROOT ]; then rmdir $WEBROOT; fi
+  ln -s /var/www $WEBROOT
+fi
+
+# Copy script to container-accessible directory
+if [ ! -f $WEBROOT/$SCRIPTNAME ]; then cp $0 $WEBROOT/$SCRIPTNAME; fi
+
+cd $WEBROOT
+if [ $? -ne 0 ]; then echo "ERROR: Could not CD to $WEBROOT"; exit 1; fi
+
+if [ -f config-dist.php ]; then mv config-dist.php config.php; fi
+if [ -f .htaccess.txt ]; then mv .htaccess.txt .htaccess; fi
+if [ -f admin/config-dist.php ]; then mv admin/config-dist.php admin/config.php; fi
+
+if [ "$(stat -c '%u:%g' index.php)" != "33:33" ]; then
+  echo Setting file ownership
+  chown -R 33:33 *
+fi
+
+# Apache config to enable SEO
+for FI in "/etc/apache2/apache2.conf" "/etc/apache2/sites-enabled/default" "/etc/httpd/conf/httpd.conf" "/etc/httpd/sites-available/default"; do
+  if [ -f $FI ]; then
+    sed -i '\|^\s*<Directory /var/www/html/>|,\|^\s*</Directory>| s|AllowOverride None|AllowOverride All|' $FI
+  fi
+done
+
+# Opencart Settings to Enable SEO
+if grep -q "'config_seo_url', '0'" $WEBROOT/install/opencart.sql; then
+  sed -i "s|'config_seo_url', '0'|'config_seo_url', '1'|" $WEBROOT/install/opencart.sql
+fi
+
+if [ ! -d /var/www-private ]; then mkdir -p /var/www-private; fi
+
+WRONG_CON_ID=$(docker ps 2> /dev/null | grep " php:5.6-apache " | head -n 1 | awk '{print $1}')
+if [ ! -z $WRONG_CON_ID ]; then
+  CIMG="ss-registry.dcv.svpod/ss-demos/php:5.6-apache-ccs"
+
+  echo Disposing of container with insufficient PHP Modules installed.
+  docker kill $WRONG_CON_ID > /dev/null; docker rm $WRONG_CON_ID > /dev/null
+  if [ ! -d /etc/docker/certs.d/ss-registry.dcv.svpod ]; then mkdir -p /etc/docker/certs.d/ss-registry.dcv.svpod; fi
+  if [ ! -f /etc/docker/certs.d/ss-registry.dcv.svpod/ca.crt ]; then
+    echo Populating ss-registry.dcv.svpod Private Registry certificate
+    openssl s_client -connect ss-registry.dcv.svpod:443 < /dev/null 2> /dev/null | openssl x509 -outform pem > /etc/docker/certs.d/ss-registry.dcv.svpod/ca.crt
+    if [ $? -ne 0 ]; then echo "ERROR: Failed to populate Private Registry certificate"; exit 1; fi
+  fi
+  echo Recreating container with sufficient PHP Modules installed.
+  if ! docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "$CIMG"; then
+    echo Pulling image $CIMG
+    docker pull $CIMG > /dev/null
+    if [ $? -ne 0 ]; then echo ERROR: Could not pull image $CIMG; exit 1; fi
+  fi
+  echo Creating new container
+  CON_NEW=$(docker create -w /var/www/html \
+    -v /etc/apache2/sites-available:/etc/apache2/sites-available \
+    -v /etc/apache2/sites-enabled:/etc/apache2/sites-enabled \
+    -v /usr/local/osmosix/cert:/etc/apache2/cert \
+    -v /etc/apache2/apache2.conf:/etc/apache2/apache2.conf \
+    -v /var/www:/var/www/html \
+    -v /var/www-private:/var/www/private \
+    -p 80:80/tcp \
+    -p 443:443/tcp \
+    --name=apache2 \
+    ss-registry.dcv.svpod/ss-demos/php:5.6-apache-ccs)
+  echo Starting container
+  docker start $CON_NEW
+
+  # Container is up?
+  er=0
+  for ISUP in $(seq 90 -1 1); do
+    CON_ID=$(docker ps | grep apache2 | head -n 1 | awk '{print $1}')
+    docker inspect $CON_ID 2> /dev/null | grep "Status" | grep -q "running"
+    er=$?
+    if [ $er -eq 0 ]; then break; fi
+    sleep 1
   done
-for j in $CliqrTier_Apache_PUBLIC_IP ;
-  do
-    ipposition=${IPINDEX}
-    let IPINDEX=${IPINDEX}+1
-    if [ $ipposition = $node_position ];
-       then
-         HTTP_SERVER=$j;
-#         echo $HTTP_SERVER;
+  if [ $er -ne 0 ]; then echo "ERROR: Container didn't come up in a timely manner"; exit 1; fi
+
+fi
+
+CON_ID=$(docker ps 2> /dev/null | grep apache2 | head -n 1 | awk '{print $1}')
+
+if [ ! -z $CON_ID ]; then
+  echo Apache Docker Container detected. Relaunching initialisation script from from within container.
+  docker exec $CON_ID /bin/bash -c "( export CliqrTier_Apache_IP=$Apache_IP; export CliqrTier_Database_IP=$Database_IP; $WEBROOT/$SCRIPTNAME )"
+  exit $?
+fi
+
+
+# When not in a container, execute the apache config changes and restart commands
+if cat /proc/1/sched | head -n 1 | egrep "^init "; then
+  if [ -f /usr/lib/systemd/system/httpd.service ]; then
+    systemctl restart httpd
+  else
+    service httpd restart
+  fi
+fi
+
+
+cd $WEBROOT/install
+
+iRetries=9
+iPause=3
+
+echo -n "Configuring Opencart ... "
+while [ $iRetries -ge 0 ]; do  
+
+  php cli_install.php install --db_driver mysqli --db_hostname $CliqrTier_Database_IP --db_username opencart --db_password opencart --db_name opencart --username admin --password admin --email youremail@example.com --agree_tnc yes --http_server http://$Apache_IP/
+
+  er=$?
+  if [ $er -eq 0 ]; then
+    echo "Success!"
+    touch $OPENCART_INSTALLED_FILE 2> /dev/null
+
+    
+    STORAGEROOT=/var/www/private
+    echo Moving Storage Dir from $WEBROOT/system to $STORAGEROOT
+    if [ -d $WEBROOT/system/storage ]; then
+      mv $WEBROOT/system/storage $STORAGEROOT/
+      if [ $? -ne 0 ]; then echo "Move of Storage Dir failed"; exit 1; fi
+      DIR_STORAGE=$STORAGEROOT/storage/
+      for VI in "DIR_STORAGE"; do
+        if [ ! -z ${!VI} ]; then
+          for FI in "$WEBROOT/config.php" "$WEBROOT/admin/config.php"; do
+            sed -i "s|^define('${VI}'.*$|define('${VI}', '${!VI}');|" $FI
+          done
+        fi
+      done
     fi
-  done
 
-#####added by vinod#######
+    echo "done"
 
-cd /var/www/install
-php cli_install.php install --db_driver mysqli --db_host $CliqrTier_Database_PUBLIC_IP --db_user root --db_password opencart --db_name opencart --username admin --password admin --email ratmistr@cisco.com --agree_tnc yes --http_server http://$HTTP_SERVER/
-touch $OPENCART_INSTALLED_FILE
+    exit 0
+  fi
+  echo "Failed! Error code: $er, retries left: $iRetries"
+  sleep $iPause
+  ((iRetries--))
+
+done
+
+echo "Failed to perform the initial Opencart configuration"
+exit 1
