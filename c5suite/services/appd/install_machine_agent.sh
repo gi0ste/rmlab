d159f67377ca8618f23e9ac6836589c76f220cbf
gi0ste <34270727+gi0ste@users.noreply.github.com>
2019-05-15 15:54:04 +0200

    Update install_machine_agent.sh

--- c5suite/services/appd/install_machine_agent.sh
+++ c5suite/services/appd/install_machine_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-exec > >(tee -a /var/tmp/appd_agent_java_$$.log) 2>&1
-set -e
+#exec > >(tee -a ./appd_agent_java_$$.log) 2>&1
+#set -e
 . /usr/local/osmosix/etc/.osmosix.sh
 . /usr/local/osmosix/etc/userenv
 . /usr/local/osmosix/service/utils/cfgutil.sh
@@ -11,33 +11,35 @@ set -e
 AGENT_LOCATION="http://www.stefanogioia.com/appdynamics" #Where to donwload the agent
 AGENT_VERSION="appdynamics-machine-agent-4.5.10.2131.x86_64.rpm"
 
-#List here akll the prerequisits
-prereqs="java"
+#List here all the prerequisits
+prereqs="java wget"
 agentSendLogMessage  "AppD: Installing OS Prereqs ${prereqs}"
 sudo yum install -y ${prereqs}
 
 
 #Downloading the AppD Agent
-echo "Downloading the AppDynamics Machine Agent..."
-cd /tmp/
-mkdir appd-agent -p
-cd /tmp/appd-agent
-wget --proxy=off ${AGENT_LOCATION}/${AGENT_VERSION}; touch /tmp/appd-agent/downloadagent.done &
+agentSendLogMessage "Downloading the AppDynamics Machine Agent..."
+wget --proxy=off ${AGENT_LOCATION}/${AGENT_VERSION}
 
 # Install the AppD Agent
-echo "Installing the AppDynamics Machine Agent..."
-rpm -ivh ${AGENT_VERSION}
-#rpm -ivh appdynamics-machine-agent-4.3.7.3-1.x86_64.rpm
+agentSendLogMessage "Installing the AppDynamics Machine Agent..."
+sudo rpm -ivh ${AGENT_VERSION}
 
-echo "Configuring the AppDynamics Machine Agent..."
-sed -i.bkp -e "s%<controller-host>%<controller-host>${APPD_CONF_CONTROLLER_HOST}%g" \
+
+agentSendLogMessage "Configuring the AppDynamics Machine Agent..."
+sudo sed -i -e "s%<controller-host>%<controller-host>${APPD_CONF_CONTROLLER_HOST}%g" \
 -e "s%<controller-port>%<controller-port>${APPD_CONF_CONTROLLER_PORT}%g" \
 -e "s%<account-access-key>%<account-access-key>${APPD_CONF_ACCESS_KEY}%g" \
--e "s%<controller-ssl-enabled>%<controller-ssl-enabled>false%g" \
 -e "s%<account-name>%<account-name>${APPD_CONF_ACCOUNT}%g" \
--e "s%<sim-enabled>false%<sim-enabled>false%g" \
 /opt/appdynamics/machine-agent/conf/controller-info.xml
 
+# Starting Machine Agent
+agentSendLogMessage "Starting the AppDynamics Machine Agent..."
+sleep 10
+sudo service appdynamics-machine-agent start
 
-# Restart Machine Agent
-echo "Starting the AppDynamics Machine Agent..."
+# Clean up the temporary file and the agent package file
+agentSendLogMessage "Cleaning files... AppDynamics Machine Agent..."
+rm -rf $AGENT_VERSION
+
+agentSendLogMessage "AppD: Machine Agent Installation done!"
