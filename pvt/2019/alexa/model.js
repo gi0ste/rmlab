816ec8765f97344c237b733755630302a8d7f279
gi0ste <34270727+gi0ste@users.noreply.github.com>
2019-08-02 15:31:19 +0200

    Create model.js

---
+++ pvt/2019/alexa/model.js
@@ -0,0 +1,34 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "cloudcenterpartner",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": []
+                },
+                {
+                    "name": "TenantIntent",
+                    "slots": [],
+                    "samples": [
+                        "how many tenants",
+                        "give me all tenants"
+                    ]
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                }
+            ],
+            "types": []
+        }
+    }
+}
